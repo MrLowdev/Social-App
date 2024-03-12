b061030f20100c61fc084b5befcc4f03614486fd
kingsuk <kingsuk@github.com>
2024-03-12 09:47:53 +0530

    fix rediract url

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,7 +16,7 @@ const NotFound = () => {
             the home page.
           </p>
           <Link
-            href="/"
+            href="/home"
             className="inline-flex text-white hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4 bg-orange-900"
           >
             Back to Homepage
